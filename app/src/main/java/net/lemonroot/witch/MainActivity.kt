47430e71f37e22b7c3cb876459cd8ce4b4e10808
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-01 22:29:38 -0400

    basic read/write test working :')

--- app/src/main/java/net/lemonroot/witch/MainActivity.kt
+++ app/src/main/java/net/lemonroot/witch/MainActivity.kt
@@ -1,11 +1,52 @@
 package net.lemonroot.witch
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import net.lemonroot.witch.databinding.ActivityFirebaseUiactivityBinding
+import net.lemonroot.witch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.readWriteTest.setOnClickListener(){
+            basicReadWrite()
+        }
+    }
+
+    private fun basicReadWrite(){
+        // [START write_message]
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        // [END read_message]
     }
 }
