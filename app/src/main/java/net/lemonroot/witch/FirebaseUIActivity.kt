3620ac3ea2eb82f35f736b7fc9e1f2947e3ff926
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-01 20:04:47 -0400

    Initial commit

---
+++ app/src/main/java/net/lemonroot/witch/FirebaseUIActivity.kt
@@ -0,0 +1,194 @@
+package net.lemonroot.witch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.ActionCodeSettings
+import com.google.firebase.auth.FirebaseAuth
+import net.lemonroot.witch.databinding.ActivityFirebaseUiactivityBinding
+import java.security.Provider
+
+class FirebaseUIActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFirebaseUiactivityBinding
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    final val AUTH_REQUEST_CODE = 7001 // Any number
+    lateinit var firebaseAuth:FirebaseAuth
+    lateinit var listener:FirebaseAuth.AuthStateListener
+    lateinit var providers:List<AuthUI.IdpConfig>
+
+    override fun onStart(){
+        super.onStart()
+        firebaseAuth.addAuthStateListener(listener)
+    }
+
+    override fun onStop(){
+        if(listener != null)
+            firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirebaseUiactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signoutBtn.setOnClickListener(){
+            // [START auth_fui_signout]
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    // ...
+                }
+            Toast.makeText(this@FirebaseUIActivity, "done", Toast.LENGTH_SHORT).show()
+            // [END auth_fui_signout]
+        }
+        init()
+    }
+
+    private fun init(){
+        providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        listener = FirebaseAuth.AuthStateListener { p0 ->
+            val user = p0.currentUser
+            if(user != null){
+                Toast.makeText(this@FirebaseUIActivity, ""+user.uid, Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this@FirebaseUIActivity, "no", Toast.LENGTH_SHORT).show()
+                startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),AUTH_REQUEST_CODE)
+            }
+        }
+    }
+
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+    // [END auth_fui_result]
+
+    private fun signOut() {
+
+    }
+
+    private fun delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_delete]
+    }
+
+    private fun themeAndLogo() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+
+        // [START auth_fui_theme_logo]
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            //.setLogo(R.drawable.my_great_logo) // Set logo drawable
+            //.setTheme(R.style.MySuperAppTheme) // Set theme
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_theme_logo]
+    }
+
+    private fun privacyAndTerms() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+        // [START auth_fui_pp_tos]
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTosAndPrivacyPolicyUrls(
+                "https://example.com/terms.html",
+                "https://example.com/privacy.html")
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_pp_tos]
+    }
+
+    open fun emailLink() {
+        // [START auth_fui_email_link]
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName( /* yourPackageName= */
+                "...",  /* installIfNotAvailable= */
+                true,  /* minimumVersion= */
+                null)
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl("https://google.com") // This URL needs to be whitelisted
+            .build()
+
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings)
+                .build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_email_link]
+    }
+
+    open fun catchEmailLink() {
+        val providers: List<AuthUI.IdpConfig> = emptyList()
+
+        // [START auth_fui_email_link_catch]
+        if (AuthUI.canHandleIntent(intent)) {
+            val extras = intent.extras ?: return
+            val link = extras.getString("email_link_sign_in")
+            if (link != null) {
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setEmailLink(link)
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+        }
+        // [END auth_fui_email_link_catch]
+    }
+}
