ed91c1861ec52ef2838c870792833e79734225e4
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-05 10:21:47 -0400

    Stable navigation drawer

--- app/src/main/java/net/lemonroot/witch/MainActivity.kt
+++ app/src/main/java/net/lemonroot/witch/MainActivity.kt
@@ -2,41 +2,53 @@ package net.lemonroot.witch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import net.lemonroot.witch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAppBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        navController = findNavController(R.id.myNavHostFragment)
-        val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf(),
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
-        binding.topToolbar.setupWithNavController(navController, appBarConfiguration)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        var drawer: DrawerLayout = binding.drawerLayout
+        var navigationView: NavigationView = binding.navView
 
-        //val navController = this.findNavController(R.id.myNavHostFragment)
-        //NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-        //NavigationUI.setupWithNavController(binding.navView, navController)
+        mAppBarConfiguration = AppBarConfiguration.Builder(
+            R.id.homeFragment)
+            .setDrawerLayout(drawer)
+            .build()
+
+        var navController: NavController = Navigation.findNavController(this, R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(navigationView, navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_drawer, menu)
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        var navController: NavController = Navigation.findNavController(this, R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/net/lemonroot/witch/NavigationDrawer.kt
@@ -0,0 +1,4 @@
+package net.lemonroot.witch
+
+class NavigationDrawer {
+}
